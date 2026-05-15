cb4518021129a3d911314a06fd1665e138632e50
Bela <brstamenkovits@gmail.com>
2026-05-15 20:59:42 +0900

    list mockapi items in UI

--- src/neutrino/lib/main.dart
+++ src/neutrino/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:neutrino/data/mock_api.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: FutureBuilder<List<FoodItem>>(
+          future: mockApi.getFood(count: 6),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+
+            final items = snapshot.data ?? [];
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (final item in items) Text(item.name),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
