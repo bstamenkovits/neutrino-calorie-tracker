d2e785823ca3642abf74e856115e1c2db515f877
Bela <brstamenkovits@gmail.com>
2026-05-27 21:34:31 +0900

    enable RLS & add policies for all tables in db

--- supabase/migrations/004__meals.sql
+++ supabase/migrations/004__meals.sql
@@ -1,8 +1,20 @@
-create table public.meals (
-    -- columns
-    id uuid not null,
+-- Create table
+create table app.meals (
+    id uuid not null default gen_random_uuid(),
     name text not null,
 
-    -- primary key
     constraint meals_pkey_id primary key (id)
 ) TABLESPACE pg_default;
+
+
+
+-- Enable RLS
+alter table app.meals enable row level security;
+
+
+-- Create RLS policies
+create policy "Allow authenticated users to read meals"
+on app.meals
+for select
+to authenticated
+using (true); -- Replace 'true' with your actual conditions if users should only see their own meals
